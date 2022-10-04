3bc4c32496569a59e0c31e06c45fd87037ff9ae0
oxavibes <bozzistef@gmail.com>
2022-10-04 16:26:52 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Infinite scroll using skeleton loading</h1>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped></style>
