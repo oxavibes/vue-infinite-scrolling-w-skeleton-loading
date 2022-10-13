6b12e35a16aec161dab4df6b07a21fd63fcf9341
oxavibes <bozzistef@gmail.com>
2022-10-13 01:40:12 -0300

    refactor: useUser composable

--- src/composables/useUser.js
+++ src/composables/useUser.js
@@ -1,16 +1,37 @@
-import { ref, readonly } from "vue";
+import { ref, computed, readonly } from "vue";
 
 export default function useUser(baseUrl) {
   let users = ref([]);
+  let isLoading = ref(false);
+
+  // How much items to load in each step
+  const stepLoad = 10;
+
+  // How many items are loaded (starts with stepLoad value)
+  const loaded = ref(0);
+
+  // The collection lazy loaded
+  const usersLoaded = computed(() => users.value.slice(0, loaded.value));
 
   let fetchUsers = async () => {
-    const { results = [] } = await (await fetch(baseUrl)).json();
+    loaded.value += stepLoad;
 
-    users.value = users.value.concat(results);
+    isLoading.value = true;
+
+    const { results = [] } = await (await fetch(`${baseUrl}`)).json();
+
+    setTimeout(() => {
+      isLoading.value = false;
+
+      users.value = [...users.value, ...results];
+    }, 500);
   };
 
   return {
+    stepLoad,
     fetchUsers,
     users: readonly(users),
+    usersLoaded: readonly(usersLoaded),
+    isLoading: readonly(isLoading),
   };
 }
