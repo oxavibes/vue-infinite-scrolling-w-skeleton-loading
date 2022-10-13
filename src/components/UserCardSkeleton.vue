30cb400acde84d3e79f3c1af763a1e54f0aff743
oxavibes <bozzistef@gmail.com>
2022-10-13 01:27:26 -0300

    feat: create UserCardSkeleton component

---
+++ src/components/UserCardSkeleton.vue
@@ -0,0 +1,31 @@
+<script setup>
+import AnimatedSkeleton from "./AnimatedSkeleton.vue";
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-content">
+      <AnimatedSkeleton
+        height="128px"
+        width="128px"
+        borderRadius="50%"
+      ></AnimatedSkeleton>
+
+      <div class="card-details">
+        <h2>
+          <AnimatedSkeleton></AnimatedSkeleton>
+        </h2>
+
+        <div class="card-details-inner">
+          <AnimatedSkeleton width="70px" height="36px"></AnimatedSkeleton>
+
+          <AnimatedSkeleton width="70px" height="36px"></AnimatedSkeleton>
+
+          <AnimatedSkeleton width="70px" height="36px"></AnimatedSkeleton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
